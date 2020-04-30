8a1541b7fd3412b54ff84a7043d1f59c4fdf7d9b
Alex Masluk <masluk.alex@gmail.com>
2020-04-29 18:24:02 -0700

    removed stuff2

--- restapi.py
+++ restapi.py
@@ -1,25 +1,17 @@
 import requests
 import json
 import string
-import requests_with_caching
 
-def get_synonym(word):
+def get_synonyms(word):
     baseurl = "https://api.datamuse.com/words"
     params_diction = {} # Set up an empty dictionary for query parameters
     params_diction["rel_syn"] = word
-    params_diction["max"] = "1" # get at most 1 results
+    params_diction["max"] = "3" # get at most 1 results
     resp = requests.get(baseurl, params=params_diction)
-    # return the top three words
-    word_ds = resp.json()
-    if word_ds != []:
-        # If we get a match, return match
-        word = word_ds[0]['word']
-    #else just return original input
-    return word
+    return resp
 
-preppedURL = requests_with_caching.requestURL('https://www.google.com/search', params = {'tab':'isch', 'q':'"violins and guitars"'})
-
-resp = requests_with_caching.get(preppedURL)
+# return the top three words
+resp = get_synonyms('funny')
 print(resp.url)
 print(resp.status_code)
-print(get_synonym('funny'))
+print(resp.json())
